e1303fb92770c042be3981e869a579bd6c436ceb
Manueljogu <ma.guara@duocuc.cl>
2024-05-26 23:05:15 -0400

    modificando el estilo de home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -28,6 +28,9 @@ export class HomePage {
   clearInputs() {
     this.user.firstName = '';
     this.user.lastName = '';
+    this.user.educationLevel = '';
+    this.user.birthDate = '';
+    
 
     if (this.firstNameInput) {
       this.animateInput(this.firstNameInput);
