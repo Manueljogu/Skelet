d3600ea60414b79f8a2e16af8300f1fd9d3469b0
Manueljogu <ma.guara@duocuc.cl>
2024-06-09 11:42:38 -0400

    creacion de 3 componentes

---
+++ src/app/components/experiencialaboral/experiencialaboral.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ExperiencialaboralComponent } from './experiencialaboral.component';
+
+describe('ExperiencialaboralComponent', () => {
+  let component: ExperiencialaboralComponent;
+  let fixture: ComponentFixture<ExperiencialaboralComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExperiencialaboralComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ExperiencialaboralComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
